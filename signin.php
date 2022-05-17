65dab1380607df967643dd203e660e85476850a0
Nethma Dhananjani <64515990+Nethma2000@users.noreply.github.com>
2022-05-17 14:46:15 +0530

    Added the job search section

--- signin.php
+++ signin.php
@@ -18,7 +18,7 @@
     <div class="container">
         <div class="forms-container">
             <div class="signin-signup">
-                <form action="signin.php" class="sign-in-form" method="post">
+                <form action="login.php" class="sign-in-form" method="post">
                     <h2 class="title">Sign in</h2>
                     <div class="input-field">
                         <i class="fas fa-user"></i>
